0b991fe733ee2d0f709eeeee9acc6fca913486a8
rthomas <rthomas015@student.butte.edu>
2017-08-25 03:10:07 +0000

    Lab1.2 Lecture Questions

---
+++ lab11/lecture1.2.cpp
@@ -0,0 +1,28 @@
+//Created By: April Browne [These are comments]
+//Created On: 8/22/2016
+
+ #include <iostream> //[These are the preprocessor directives]
+ using namespace std;
+
+
+ int main() // This is the main function
+
+ {
+
+     cout << "Hello World!" << endl << "Hello Robert!" << endl;
+
+ }
+
+ 
+
+ //Identify the following parts of the program: [These are comments]
+
+ //   preprocessor directives
+
+ //   main function
+
+ //   comments
+
+ //update it so that it is human readable
+
+ //add a second output line that says "Hello YourName!"
