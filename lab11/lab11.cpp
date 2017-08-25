7624bf9c599d31344b88f26bae36dc6393e5929f
rthomas <rthomas015@student.butte.edu>
2017-08-25 02:05:56 +0000

    Lab1.1 submimssion

---
+++ lab11/lab11.cpp
@@ -0,0 +1,21 @@
+/*  Lab 1.1 - Algorithms
+ *  Simple Number Guessing Game
+ *
+    Step 1: The computer randomly selects a number in a predetermined range
+    Step 2: The computer will create a 'points counter' set to 10 starting points
+    Step 3: Computer outputs a statement 'Please guess a number between
+            [lower boundary of predetermined range] and [higher boundary of predetermined range] 
+    Step 4: Computer queries user for input
+    Step 5: Computer takes the number and stores the information
+    Step 6: Computer checks to see if the number provided by user falls into the predetermined range
+    Step 7: If the input does not fall into the correct predetermined range, then
+            the computer will repeat starting at step 2 otherwise the number is valid
+    Step 8: The computer will subtract one from the 'point counter'
+    Step 9: The computer now compares the user input to the random number it selected, if the
+            guess is more than the random number the computer will output the text "lower", 
+            if the guess is less than the random number the computer will output "higher", and
+            if the guess is equal to the random number it will output "correct!"
+    Step 10: The computer will output the users score
+ *
+ *
+ */
