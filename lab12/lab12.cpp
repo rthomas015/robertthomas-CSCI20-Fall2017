31aa71650da194b1c9a2cb1b0ff34109afccc0aa
rthomas <rthomas015@student.butte.edu>
2017-08-30 01:42:32 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,39 @@
+//Lab 1.2 - ASCII Art
+//By: Robert Thomas
+//Date: 8/29/2017
+//ASCII sword (kind of) using output stream to render
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    cout << "       /|\\           " << endl;
+    cout << "      / | \\          " << endl;
+    cout << "     /  |  \\         " << endl;
+    cout << "     |  /\\  |        " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "     | |  | |         " << endl;
+    cout << "   ═ | |  | | ═       " << endl;
+    cout << "{==[+  \\ + /  +]==}  " << endl;
+    cout << "    \\========/        " << endl;
+    cout << "     \\|****|/         " << endl;
+    cout << "      |****|         " << endl;
+    cout << "      |****|         " << endl;
+    cout << "      |****|         " << endl;
+    cout << "      |****|         " << endl;
+    cout << "      |****|         " << endl;
+    cout << "      /____\\         " << endl;
+    cout << "      \\____/         " << endl;
+    
+    return 0;
+}
