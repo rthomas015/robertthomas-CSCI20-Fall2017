67ae45037d56588fedc79d9c9713f9a65bcb5958
rthomas <rthomas015@student.butte.edu>
2017-09-06 06:38:57 +0000

    Lab1.4 Commit

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -13,8 +13,8 @@ using namespace std;
 //start of program
 int main () {
     
-    int cents_deposited = 0, total_quarters = 0, total_dimes = 0, total_nickles = 0, total_pennies = 0, total_dollars_before_fee = 0, total_cents_before_fee = 0, total_dollars_after_fee = 0, total_cents_after_fee = 0;
-    double total_after_fee = 0.0, total_before_fee = 0.0;
+    int cents_deposited = 0, total_quarters = 0, total_dimes = 0, total_nickles = 0, total_pennies = 0;
+    double total_after_fee = 0.00, total_before_fee = 0.00;
     double const usage_fee = 0.109; 
 
     //Query information from client
@@ -30,23 +30,12 @@ int main () {
     total_pennies = ((cents_deposited % 25) % 10) % 5;
     
     /*Calculate the amount in dollars/cents after fee is applied
-      Questionable way to round, since it doesn't account for actual rounding rules, just truncates, which 
+      Note: Questionable way to round, since it doesn't account for actual rounding rules, just truncates, which 
       is why TD Bank is paying out money from a lawsuit for its coin counting machines
     */
     
-    total_dollars_before_fee = cents_deposited / 100;
-    total_cents_before_fee = cents_deposited;
-    total_cents_before_fee = cents_deposited % 100;
-    
-    total_dollars_after_fee = (cents_deposited - (cents_deposited * usage_fee)) / 100;
-    total_cents_after_fee = cents_deposited - (cents_deposited * usage_fee);
-    total_cents_after_fee = total_cents_after_fee % 100;
-   
-    /*These equations work the same, but didn't utilize modulo:
-      total_dimes = (total_cents_deposited - (total_quarters * 25)) / 10;
-      total_nickles = (total_cents_deposited - ((total_quarters * 25) + (total_dimes * 10))) / 5;
-      total_pennies = total_cents_deposited - ((total_quarters * 25) + (total_dimes * 10) + (total_nickles * 5));
-    */
+    total_before_fee = cents_deposited / 100.0;
+    total_after_fee = static_cast<int>((cents_deposited - (cents_deposited * usage_fee))) / 100.0;
     
     //Output information to user
     cout << endl 
@@ -55,10 +44,27 @@ int main () {
     << " " << total_nickles << " nickles, "
     << "and " << total_pennies << " pennies." << endl;
     
-    cout << endl << "The total amount deposited: $" << total_dollars_before_fee << "." << total_cents_before_fee << endl;
-    cout << endl << "Total cash after fee: $" << total_dollars_after_fee << "." << total_cents_after_fee << endl;
+    //cout << endl << test << endl;
+    cout << endl << "The total amount deposited: $" << total_before_fee << endl;
+    cout << endl << "Total cash after fee: $" << total_after_fee << endl << endl;
     
     cout << "Please print this screen to use as a voucher.";
     
     return 0;
 }
+
+
+/*
+      //These equations work the same to figure out exact change, but didn't utilize modulo:
+      
+      total_dimes = (total_cents_deposited - (total_quarters * 25)) / 10;
+      total_nickles = (total_cents_deposited - ((total_quarters * 25) + (total_dimes * 10))) / 5;
+      total_pennies = total_cents_deposited - ((total_quarters * 25) + (total_dimes * 10) + (total_nickles * 5));
+    
+      //These equations work the same to figure out dollars and cents for total amount deposited, but were more convoluted
+      
+      total_cents_before_fee = cents_deposited % 100;
+      total_dollars_before_fee = cents_deposited / 100;
+      
+      total_dollars_after_fee = (cents_deposited - (cents_deposited * usage_fee)) / 100;
+    */
