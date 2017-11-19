4caa8dbbfd1b9024c85d3905bf37506490cf9369
rthomas <rthomas015@student.butte.edu>
2017-11-19 03:58:00 +0000

    Lab 4.6 - submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -1,13 +1,14 @@
 /*Lab 4.6 - File Input/Output
   By: Robert Thomas
   Date: 11/16/2017
-  Description: This
+  Description: This program will take input from a text file and 
 */
 
 //preprocessor directives
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -26,7 +27,9 @@ int main () {
     double grade_point_average = 0.0;
     
     html_output.open("index.html");
+    grade_file.open("input.txt");
     
+   /* //Request input file's name  
     cout << "Please enter the name of the grade file: ";
     cin >> grade_file_name;
     
@@ -37,29 +40,30 @@ int main () {
         cout << endl << "The file you attempted to open doesn't exist or is not accessible, please input another: ";
         cin >> grade_file_name;
         grade_file.open(grade_file_name);
-    }
-    
+    }*/
+
+  
     bool line_is_a_name = true;
-    html_output << "<HTML><HEAD><TITLE>List of Grades</TITLE></HEAD><BODY><BR><BR><H1>List of Student Grades:</H1>" << endl;
+    html_output << "<HTML><HEAD><TITLE>List of Grades</TITLE></HEAD><BODY><BR><BR><H1>List of Student Grades</H1>";
+    html_output << "<PRE>Name:                  Class #:    Units:    Grade:</PRE>";
    
     while (!grade_file.eof()) {
+            //if this part of the file is a name
             if (line_is_a_name == true) {
                 getline(grade_file, file_contents);
-                //then its a name
-                html_output << "Name: " << file_contents << "<BR>" << endl;
+                html_output << "<PRE>" << file_contents << endl;
                 line_is_a_name = false;
             }
             else if (line_is_a_name == false) {
-                //then it is the number of classes
-                cin.ignore();
+                //then it is the number of classes that person is taking
+                //cin.ignore(); //clear stream
                 grade_file >> number_of_classes;
                 //use the number of classes as a loop and
                 //loop through the next line
                     for (int i=1;i<=number_of_classes;i++) {
                         grade_file >> number_of_units;
                         grade_file >> grade_letter;
-                        cout << endl << "<br>" << "Class #" << i << ": " << grade_letter << endl
-                             << "<br> Number of Units: " << number_of_units;
+                        html_output << endl << "<br>                               " << i << "      " << number_of_units << "      " << grade_letter << endl;
                         total_units = total_units + number_of_units;
                         switch (grade_letter) {
                             case 'A':
@@ -81,19 +85,19 @@ int main () {
 
                     }
                 grade_point_average = (grade_point_average / total_units);
-                cout << "GPA: " << grade_point_average;
-                //grade_point_average = 0;
-                grade_file >> file_contents;
+                html_output << "<br>                                                    GPA: " << grade_point_average << setprecision(3) << "</PRE><BR>";
+                grade_point_average = 0.0;
+                total_units = 0;
+                grade_file.ignore();
+                getline(grade_file, file_contents);
                 line_is_a_name = true;
             }
-    }
+    } //reached end of file
     
 
     
-    /*
- html_output << file_contents << endl;
     html_output << "</BODY></HTML>";
-    */
+
     grade_file.close();
     html_output.close();
     
