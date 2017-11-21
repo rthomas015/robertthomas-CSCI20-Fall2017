31be8d1c46fa0a338fb72486f42ab958cd182c3b
rthomas <rthomas015@student.butte.edu>
2017-11-21 04:21:45 +0000

    Lab 4.6 - submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -1,7 +1,8 @@
 /*Lab 4.6 - File Input/Output
   By: Robert Thomas
   Date: 11/16/2017
-  Description: This program will take input from a text file and 
+  Description: This program will take input from a text file and parse it, and output the information to
+  an html file with some minimal formatting to make it readable.
 */
 
 //preprocessor directives
@@ -14,12 +15,18 @@ using namespace std;
 
 //start of main program
 int main () {
+    //create input file stream object named grade_file
     ifstream grade_file;
+    //create an output file stream object named html_output
     ofstream html_output;
+    //grade_file_name - holds the grade file's name/location 
+    //file_conents - string that holds the information retrieved from the ifstream
     string grade_file_name = "",
            file_contents = "";
+    //grade_letter - a char that holds the grade letter of the student retreived from the ifstream
     char grade_letter;
            
+    //number_of_classes - integer holding the number of classes
     int number_of_classes = 0,
         total_units = 0,
         number_of_units = 0;
@@ -27,9 +34,8 @@ int main () {
     double grade_point_average = 0.0;
     
     html_output.open("index.html");
-    grade_file.open("input.txt");
     
-   /* //Request input file's name  
+   //Request input file's name  
     cout << "Please enter the name of the grade file: ";
     cin >> grade_file_name;
     
@@ -40,7 +46,7 @@ int main () {
         cout << endl << "The file you attempted to open doesn't exist or is not accessible, please input another: ";
         cin >> grade_file_name;
         grade_file.open(grade_file_name);
-    }*/
+    }
 
   
     bool line_is_a_name = true;
@@ -104,6 +110,38 @@ int main () {
     
     //Output:
     /*
+    Program outputs to HTML file:
+    
+    <HTML><HEAD><TITLE>List of Grades</TITLE></HEAD><BODY><BR><BR><H1>List of Student Grades</H1><PRE>Name:                  Class #:    Units:    Grade:</PRE><PRE>Suzy Cue
+
+<br>                               1      3      A
+
+<br>                               2      3      A
+
+<br>                               3      3      A
+
+<br>                               4      3      B
+<br>                                                    GPA: 3.75</PRE><BR><PRE>Joe Joseph
+
+<br>                               1      4      D
+
+<br>                               2      3      C
+
+<br>                               3      3      B
+
+<br>                               4      3      C
+<br>                                                    GPA: 1.92</PRE><BR><PRE>Jillian Smith
+
+<br>                               1      4      C
+
+<br>                               2      3      C
+
+<br>                               3      4      C
+
+<br>                               4      3      B
+
+<br>                               5      3      C
+<br>                                                    GPA: 2.18</PRE><BR></BODY></HTML>
      */
     
     return 0;
